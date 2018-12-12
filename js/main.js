9cce085e8a9ad9279960b2a5c3685ca8fd52f469
EthanJGibson <43793072+abass20@users.noreply.github.com>
2018-12-12 10:55:21 -0600

    Put all files in

---
+++ js/main.js
@@ -0,0 +1,20 @@
+class Chesspiece {
+    constructor(type, pos, team, turn, check, checkmate, isDead) {
+        this.type = type
+        this.pos = pos
+        this.team = team
+        this.turn = turn
+        this.check = check
+        this.checkmate = checkmate
+        this.isDead = isDead
+    }
+}
+
+const piece1 = new Chesspiece("rook", "A1", "black", "1", false, false, false)
+const piece2 = new Chesspiece("pawn", "A1", "black", "1", false, false, false)
+const piece3 = new Chesspiece("bishop", "A1", "black", "1", false, false, false)
+const piece4 = new Chesspiece("queen", "A1", "black", "1", false, false, false)
+const piece5 = new Chesspiece("king", "A1", "black", "1", false, false, false)
+const piece6 = new Chesspiece("knight", "A1", "black", "1", false, false, false)
+
+console.log(piece1);
