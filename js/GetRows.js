00a71bf97440229c94253aeb2db597d95baee4ba
FalseUtopian <swordman211@yahoo.com>
2018-12-12 10:58:30 -0600

    Column

--- js/GetRows.js
+++ js/GetRows.js
@@ -1,7 +1,14 @@
-let string = "a7";
+let string = "A7";
+
+function getColumn(str) {
+    index = 0;
+    return str.charAt(index);
+}
+console.log(getColumn(string)); 
 
 function getRow(str) {
     index = 1;
     return str.charAt(index);
 }
 console.log(getRow(string));
+
