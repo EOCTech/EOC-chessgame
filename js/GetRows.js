fdd747ac265e16cbc0e98ab8e1e6e79a00cb41f0
FalseUtopian <swordman211@yahoo.com>
2018-12-12 10:34:10 -0600

    Create GetRows.js

---
+++ js/GetRows.js
@@ -0,0 +1,10 @@
+let string = "a7";
+
+function getRow(str) {
+    index = 2;
+    return getRow = getRow.charAt(index);
+    // take string, and get the second character
+
+    // return the result
+}
+console.log(getRow);
