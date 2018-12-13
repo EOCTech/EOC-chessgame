ae07e82d51007a887ab2d781672a0eea1e691170
EthanJGibson <43793072+abass20@users.noreply.github.com>
2018-12-13 08:49:46 -0600

    moved checkForDead to main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+function checkForDead(objArr) {
+    //console.log(objArr[2].isDead);
+    let deadObjects = [];
+
+    // iterate through the array of objects
+    for( i = 0; i < objArr.length; i += 1) {
+        // if its dead, add it to a new array called "deadObjects"
+        if (objArr[i].isDead == true) {
+            deadObjects.push(objArr[i]);
+        }
+    }
+    return deadObjects;
+}
