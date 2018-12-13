2bf4a3b3d21dcefa62e7eb6c9c3561f2088e0259
EthanJGibson <43793072+abass20@users.noreply.github.com>
2018-12-13 09:56:50 -0600

    board is in the index now

--- js/main.js
+++ js/main.js
@@ -11,43 +11,51 @@ class Chesspiece {
     }
 }
 
-const rook1 = new Chesspiece("rook", "a1", "white", "1", false, false, false)
-const rook2 = new Chesspiece("rook", "h1", "white", "1", false, false, false)
-const rook3 = new Chesspiece("rook", "a8", "black", "1", false, false, false)
-const rook4 = new Chesspiece("rook", "h8", "black", "1", false, false, false)
+let chessPieces = [];
+/* create all objects code*/
+const rook1 = new Chesspiece("rook", "a1", "white", "1", false, false, false);
+chessPieces.push(rook1);
+const rook2 = new Chesspiece("rook", "h1", "white", "1", false, false, false);
+chessPieces.push(rook2);
+const rook3 = new Chesspiece("rook", "a8", "black", "1", false, false, false);
+chessPieces.push(rook3);
+const rook4 = new Chesspiece("rook", "h8", "black", "1", false, false, false);
+chessPieces.push(rook4);
 
-const pawn1 = new Chesspiece("pawn", "a2", "white", "1", false, false, false)
-const pawn2 = new Chesspiece("pawn", "b2", "white", "1", false, false, false)
-const pawn3 = new Chesspiece("pawn", "c2", "white", "1", false, false, false)
-const pawn4 = new Chesspiece("pawn", "d2", "white", "1", false, false, false)
-const pawn5 = new Chesspiece("pawn", "e2", "white", "1", false, false, false)
-const pawn6 = new Chesspiece("pawn", "f2", "white", "1", false, false, false)
-const pawn7 = new Chesspiece("pawn", "g2", "white", "1", false, false, false)
-const pawn8 = new Chesspiece("pawn", "h2", "white", "1", false, false, false)
-const pawn9 = new Chesspiece("pawn", "a7", "black", "1", false, false, false)
-const pawn10 = new Chesspiece("pawn", "b7", "black", "1", false, false, false)
-const pawn11 = new Chesspiece("pawn", "c7", "black", "1", false, false, false)
-const pawn12 = new Chesspiece("pawn", "d7", "black", "1", false, false, false)
-const pawn13 = new Chesspiece("pawn", "e7", "black", "1", false, false, false)
-const pawn14 = new Chesspiece("pawn", "f7", "black", "1", false, false, false)
-const pawn15 = new Chesspiece("pawn", "g7", "black", "1", false, false, false)
-const pawn16 = new Chesspiece("pawn", "h7", "black", "1", false, false, false)
+const pawn1 = new Chesspiece("pawn", "a2", "white", "1", false, false, false);
+chessPieces.push(pawn1);
+const pawn2 = new Chesspiece("pawn", "b2", "white", "1", false, false, false);
+chessPieces.push(pawn2);
+const pawn3 = new Chesspiece("pawn", "c2", "white", "1", false, false, false);
+const pawn4 = new Chesspiece("pawn", "d2", "white", "1", false, false, false);
+const pawn5 = new Chesspiece("pawn", "e2", "white", "1", false, false, false);
+const pawn6 = new Chesspiece("pawn", "f2", "white", "1", false, false, false);
+const pawn7 = new Chesspiece("pawn", "g2", "white", "1", false, false, false);
+const pawn8 = new Chesspiece("pawn", "h2", "white", "1", false, false, false);
+const pawn9 = new Chesspiece("pawn", "a7", "black", "1", false, false, false);
+const pawn10 = new Chesspiece("pawn", "b7", "black", "1", false, false, false);
+const pawn11 = new Chesspiece("pawn", "c7", "black", "1", false, false, false);
+const pawn12 = new Chesspiece("pawn", "d7", "black", "1", false, false, false);
+const pawn13 = new Chesspiece("pawn", "e7", "black", "1", false, false, false);
+const pawn14 = new Chesspiece("pawn", "f7", "black", "1", false, false, false);
+const pawn15 = new Chesspiece("pawn", "g7", "black", "1", false, false, false);
+const pawn16 = new Chesspiece("pawn", "h7", "black", "1", false, false, false);
 
-const bishop1 = new Chesspiece("bishop", "c1", "white", "1", false, false, false)
-const bishop2 = new Chesspiece("bishop", "f1", "white", "1", false, false, false)
-const bishop3 = new Chesspiece("bishop", "c8", "black", "1", false, false, false)
-const bishop4 = new Chesspiece("bishop", "f8", "black", "1", false, false, false)
+const bishop1 = new Chesspiece("bishop", "c1", "white", "1", false, false, false);
+const bishop2 = new Chesspiece("bishop", "f1", "white", "1", false, false, false);
+const bishop3 = new Chesspiece("bishop", "c8", "black", "1", false, false, false);
+const bishop4 = new Chesspiece("bishop", "f8", "black", "1", false, false, false);
 
-const queen1 = new Chesspiece("queen", "e1", "white", "1", false, false, false)
-const queen2 = new Chesspiece("queen", "e8", "black", "1", false, false, false)
+const queen1 = new Chesspiece("queen", "e1", "white", "1", false, false, false);
+const queen2 = new Chesspiece("queen", "e8", "black", "1", false, false, false);
 
-const king1 = new Chesspiece("king", "d1", "white", "1", false, false, false)
-const king1 = new Chesspiece("king", "d8", "black", "1", false, false, false)
+const king1 = new Chesspiece("king", "d1", "white", "1", false, false, false);
+const king2 = new Chesspiece("king", "d8", "black", "1", false, false, false);
 
-const knight1 = new Chesspiece("knight", "b1", "white", "1", false, false, false)
-const knight1 = new Chesspiece("knight", "g1", "white", "1", false, false, false)
-const knight1 = new Chesspiece("knight", "b8", "black", "1", false, false, false)
-const knight1 = new Chesspiece("knight", "g8", "black", "1", false, false, false)
+const knight1 = new Chesspiece("knight", "b1", "white", "1", false, false, false);
+const knight2 = new Chesspiece("knight", "g1", "white", "1", false, false, false);
+const knight3 = new Chesspiece("knight", "b8", "black", "1", false, false, false);
+const knight4 = new Chesspiece("knight", "g8", "black", "1", false, false, false);
 
 /* Gavin getRow and getColumn code */
 function getColumn(str) {
@@ -59,7 +67,6 @@ function getRow(str) {
     index = 1;
     return str.charAt(index);
 }
-console.log(getRow(string));
 /* Trey checkForDead code*/
 function checkForDead(objArr) {
     //console.log(objArr[2].isDead);
